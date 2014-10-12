3d0c90edbf0b4af4cbdfbbd99d10c162c1019532
Haresh Siva <haresh.siva12@gmail.com>
2014-10-12 00:17:36 -0400

    Deleting Old Files

--- PBL-Dining-Splash-Page/js/script.js
+++
@@ -1,8 +0,0 @@
-/* Smooth scrollTo */
-var scrollTo = function(el, off){
-$('html, body').animate({
-scrollTop: $(el).offset().top - off
-}, 650);
-}
-$("#header__more").click(function(){ scrollTo("#about", 0); });
-$("#footer__top").click(function(){ scrollTo("body", 0); });
